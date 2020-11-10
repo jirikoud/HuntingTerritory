bd7ca01039b38b6c909c3e9951a84aea4db7f707
Jiří Koudelka <chromiumcz@gmail.com>
2020-11-10 21:23:01 +0100

    HuntingCoreModel

--- HuntingCoreModel/Infrastructure/ContextUtils.cs
+++ HuntingCoreModel/Infrastructure/ContextUtils.cs
@@ -6,6 +6,7 @@ using System.IO;
 using System.Globalization;
 using System.Web;
 using HuntingCoreModel.Database;
+using HuntingCoreModel.Database.Models;
 
 namespace HuntingCoreModel.Infrastructure
 {
